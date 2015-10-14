d93c88cb81318035e2ccb898656f059c71b1dc4b
Adrián Moreno <admoreno@outlook.com>
2015-10-14 23:56:08 +0000

    Backend removed

--- gulpfile.js
+++ gulpfile.js
@@ -16,45 +16,45 @@ gulp.task("default", ["build"], function(){});
 
 /* IMG TASKS */
 gulp.task("move-img", function(){
-    return gulp.src("./public/src/img/*")
-        .pipe(gulp.dest("./public/dist/img"))
+    return gulp.src("./src/img/*")
+        .pipe(gulp.dest("./dist/img"))
 });
 
 /* JSON TASKS */
 //gulp.task("move:json", function(){
-//    return gulp.src("./public/src/**/*.json")
-//        .pipe(gulp.dest("./public/dist/"))
+//    return gulp.src("./src/**/*.json")
+//        .pipe(gulp.dest("./dist/"))
 //});
 
 /* CSS TASKS */
 gulp.task('clean-css', function (cb) {
-    del(["./public/dist/**/*.css"], cb);
+    del(["./dist/**/*.css"], cb);
 });
 
 gulp.task("css", ["clean-css"], function(){
-    return gulp.src("./public/src/**/*.css")
+    return gulp.src("./src/**/*.css")
         .pipe(cssmin())
         .pipe(rename({
             extname: ".min.css"
         }))
-        .pipe(gulp.dest("./public/dist/"))
+        .pipe(gulp.dest("./dist/"))
 });
 
 /* COFFEESCRIPT TASKS */
 gulp.task('clean-js', function (cb) {
-    del(['./public/dist/**/*.js'], cb);
+    del(['./dist/**/*.js'], cb);
 });
 
 gulp.task("coffee:dev", ["clean-js"], function() {
-    return gulp.src("./public/src/**/*.coffee")
+    return gulp.src("./src/**/*.coffee")
         .pipe(maps.init())
         .pipe(coffee({bare: true}).on('error', util.log))
         .pipe(maps.write())
-        .pipe(gulp.dest("./public/dist/"))
+        .pipe(gulp.dest("./dist/"))
 });
 
 gulp.task("coffee", ["clean-js"], function() {
-    return gulp.src("./public/src/**/*.coffee")
+    return gulp.src("./src/**/*.coffee")
         .pipe(maps.init())
         .pipe(coffee({bare: true}).on('error', util.log))
         .pipe(uglify({mangle:false}))
@@ -62,30 +62,30 @@ gulp.task("coffee", ["clean-js"], function() {
             extname: ".min.js"
         }).on('error', util.log))
         .pipe(maps.write())
-        .pipe(gulp.dest("./public/dist/"))
+        .pipe(gulp.dest("./dist/"))
 });
 
 /* HTML TASKS */
 gulp.task("clean-html", function(cb) {
-    del(['./public/dist/**/*.html'], cb);
+    del(['./dist/**/*.html'], cb);
 });
 
 gulp.task("move-html", ["clean-html"], function(){
-    return gulp.src("./public/src/**/*.html")
-        .pipe(gulp.dest("./public/dist/"))
+    return gulp.src("./src/**/*.html")
+        .pipe(gulp.dest("./dist/"))
 });
 
 /* BUILD TASKS */
 gulp.task("build:dev", ["move-html", "move-img", /*"move:json", */ "css", "coffee:dev"], function(){
-    var sources = gulp.src(['./public/dist/**/*.js', './public/dist/**/*.css'], {read: false});
-    gulp.src("./public/dist/**/*.html")
+    var sources = gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false});
+    gulp.src("./dist/**/*.html")
         .pipe(inject(sources, {relative: true}))
-        .pipe(gulp.dest("./public/dist"))
+        .pipe(gulp.dest("./dist"))
 });
 
 gulp.task("build", ["move-html", "move-img", /*"move:json",*/ "css", "coffee"], function(){
-    var sources = gulp.src(['./public/dist/**/*.js', './public/dist/**/*.css'], {read: false});
-    gulp.src("./public/dist/**/*.html")
+    var sources = gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false});
+    gulp.src("./dist/**/*.html")
         .pipe(inject(sources, {relative: true}))
         .pipe(htmlmin({
             empty: true,
@@ -96,13 +96,13 @@ gulp.task("build", ["move-html", "move-img", /*"move:json",*/ "css", "coffee"], 
             quotes: false,
             loose: true
         }))
-        .pipe(gulp.dest("./public/dist"))
+        .pipe(gulp.dest("./dist"))
 });
 
 gulp.task("launch", ["build"], function() {
-    gulp.src("./public/dist/index.html").pipe(open("", {url:"http://localhost:8082"}))
+    gulp.src("./dist/index.html").pipe(open("", {url:"http://localhost:8082"}))
 });
 
 gulp.task("launch:dev", ["build:dev"], function() {
-    gulp.src("./public/dist/index.html").pipe(open("", {url:"http://localhost:8082"}))
+    gulp.src("./dist/index.html").pipe(open("", {url:"http://localhost:8082"}))
 });
